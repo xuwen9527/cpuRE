447b050a15375aaf3a7ccebfb5b5792ac20eb866
xu wen <wxu@hexaflake.ai>
2022-10-31 18:26:49 +0800

    refactor stamp shading

--- src/pipeline/stamp_shading.h
+++ src/pipeline/stamp_shading.h
@@ -10,19 +10,20 @@ namespace cpuRE {
   template <typename FragmentShader>
   struct StampShading {
     static void run(const glm::ivec2& fragment, const glm::mat3& m, const glm::vec3& uz, Context& context) {
-      auto z_dest = BufferIO::readDepth(fragment.x, fragment.y, context.depth_buffer, context.viewport.z);
       auto p = clipcoordsFromRaster(fragment.x, fragment.y, context.pixel_scale);
-
       auto z = glm::dot(uz, p);
-      if (z >= -1.f && z < z_dest) {
-        auto u = p * m;
-        u /= u.x + u.y + u.z;
+      if (z >= -1.f) {
+        auto z_dest = BufferIO::readDepth(fragment.x, fragment.y, context.depth_buffer, context.viewport.z);
+        if(z < z_dest) {
+          auto u = p * m;
+          u /= u.x + u.y + u.z;
 
-        FragmentShader shader;
-        auto color = shader(u);
-        if (!shader.discarded()) {
-          BufferIO::writeColor(fragment.x, fragment.y, color, context.color_buffer, context.viewport.z);
-          BufferIO::writeDepth(fragment.x, fragment.y, z,     context.depth_buffer, context.viewport.z);
+          FragmentShader shader;
+          auto color = shader(u);
+          if (!shader.discarded()) {
+            BufferIO::writeColor(fragment.x, fragment.y, color, context.color_buffer, context.viewport.z);
+            BufferIO::writeDepth(fragment.x, fragment.y, z,     context.depth_buffer, context.viewport.z);
+          }
         }
       }
     }
